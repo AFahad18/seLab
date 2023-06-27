743381a6c0b6127d51afc22c572f19ef07510f52
Hasibul Hasan Rupok <rhrupok34@gmail.com>
2023-06-27 09:43:52 +0600

    templates, views, urls & installed apps updated/created

--- env/djangoProject/TourPlanner/views.py
+++ env/djangoProject/TourPlanner/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+# from .models import Post
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'html/home.html')
