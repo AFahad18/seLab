d5c4927ddaddb7a8d0bba719da2b79e76acd58e1
AFahad18 <61990228+AFahad18@users.noreply.github.com>
2023-08-27 02:37:56 +0600

    tour guide
    login successfully done.(Mithila)

--- env/djangoProject/t_logsign/urls.py
+++ env/djangoProject/t_logsign/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.SignupPage, name='signup'),
     path('login/', views.LoginPage, name='login'),
-    # path('checkview', views.checkview, name='checkview'),
+    # path('home/', views.HomePage, name='home'),
     # path('send', views.send, name='send'),
     # path('getMessages/<str:room>/', views.getMessages, name='getMessages'),
 ]
--- env/djangoProject/t_logsign/views.py
+++ env/djangoProject/t_logsign/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render,HttpResponse,redirect
 from .models import Guide
 # Create your views here.
+
+def authenticate_guide(username, password):
+    try:
+        guide = Guide.objects.get(name=username)
+        if guide.password == password:
+            return guide
+    except Guide.DoesNotExist:
+        return None
+
+
 def SignupPage(request):
     if request.method=='POST':
         uname=request.POST.get('username')
@@ -16,9 +26,18 @@ def SignupPage(request):
             my_user.save()
             # return HttpResponse("User has been created Successfully")
             # print(uname,email,location,number,pass1,pass2)
-            return redirect('html/home.html')
+            return redirect('/')
     return render (request, 'html/signup.html')
 
 def LoginPage(request):
+    if request.method=='POST':
+        username = request.POST.get('username')
+        pass1 = request.POST.get('pass')
+        user=authenticate_guide(username, pass1)
+        print(user)
+        if user is not None:
+            return redirect('/')
+        else:
+            return HttpResponse("Username and Password is incorrect!!!")
     return render (request, 'html/login.html')
 
