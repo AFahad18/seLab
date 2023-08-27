75488676a90728dfde35c38697301841a7adbba0
AFahad18 <61990228+AFahad18@users.noreply.github.com>
2023-08-27 14:23:33 +0600

    All update done
    search and shows done

--- env/djangoProject/t_logsign/urls.py
+++ env/djangoProject/t_logsign/urls.py
@@ -4,8 +4,9 @@ app_name = 't_logsign'
 urlpatterns = [
     path('signup/', views.SignupPage, name='signup'),
     path('login/', views.LoginPage, name='login'),
-     path('search/', views.search, name='search'),  # New view for the search form
+    path('search/', views.search, name='search'),  # New view for the search form
     path('search_results/', views.search_results, name='search_results'),
+    
     # path('send', views.send, name='send'),
     # path('getMessages/<str:room>/', views.getMessages, name='getMessages'),
 ]
